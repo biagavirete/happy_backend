13f3a961ab60e02a6f5035b288b644ee133fb3e8
biagavirete <biagavirete@gmail.com>
2020-10-13 11:21:56 -0300

    refactor controller and routes

--- src/database/migrations/1602594648687-create_orphanages.ts
+++ src/database/migrations/1602594648687-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1602594648687 implements MigrationInterface {
             type: 'text',
           },
           {
+            name: 'opening_hours',
+            type: 'varchar',
+          },
+          {
             name: 'open_on_weekends',
             type: 'boolean',
             default: false,
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 import './database/connection';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/users', (request, response) => {
-    return response.json({ message: 'Hello World'});
-})
+app.use(routes);
 
 app.listen(3333);
